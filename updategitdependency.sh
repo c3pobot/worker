fcb37c1de573822a76ec509204b46fb5f69cfbe9
scuba <scuba75@gmail.com>
2023-06-22 14:32:25 +0000

    update

---
+++ updategitdependency.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo updating discordapiwrapper
+npm i --package-lock-only github:/c3pobot/discordapiwrapper
+echo updating mongowrapper
+npm i --package-lock-only github:/c3pobot/mongowrapper
+echo updating quewrapper
+npm i --package-lock-only github:/c3pobot/quewrapper
+echo updating rediswrapper
+npm i --package-lock-only github:/c3pobot/rediswrapper
+echo updating socketclient
+npm i --package-lock-only github:/c3pobot/socketclient

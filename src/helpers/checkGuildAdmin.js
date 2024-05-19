a237495123877cbe1c2c78b894de8f36756ad23f
scuba75 <scuba75@gmail.com>
2024-05-18 20:51:35 -0500

    update

--- src/helpers/checkGuildAdmin.js
+++ src/helpers/checkGuildAdmin.js
@@ -3,11 +3,14 @@ const mongo = require('mongoclient')
 const getGuildId = require('./getGuildId')
 const checkServerAdmin = async(obj = {})=>{
   if(!obj.member?.user?.id) return [ false, null]
-  if(obj.guild?.owner_id == obj.member.user.id) return [ true, null]
+  //if(obj.guild?.owner_id == obj.member.user.id) return [ true, null]
+
   let server = (await mongo.find('discordServer', { _id: obj.guild_id }, { admin:1, guilds: 1 }))[0]
   if(!server?.admin || server?.admin?.length == 0) return [ false, null ]
-  let roles = obj.members?.roles
-  if(!roles || roles?.length == 0) return
+
+  let roles = obj.member?.roles
+  if(!roles || roles?.length == 0) return [ false, null ]
+
   if(server.admin.filter(x=>roles.includes(x.id)).length > 0) return [ true, server ]
 }
 

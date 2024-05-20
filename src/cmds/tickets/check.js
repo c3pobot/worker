4de02011fe587819efa80e560887e61a0bca9a43
scuba75 <scuba75@gmail.com>
2024-05-19 19:23:28 -0500

    update

--- src/cmds/tickets/check.js
+++ src/cmds/tickets/check.js
@@ -7,20 +7,14 @@ module.exports = async(obj = {}, opt = {})=>{
   let pObj = await getGuildId({dId: obj.member.user.id}, {}, opt)
   if(!pObj?.guildId) return { content: 'Error getting player guild' }
 
-  let mongoCache = 0
-  let gObj = (await mongo.find('ticketCheckCache', {_id: pObj.guildId}))[0]
-  if(gObj) mongoCache++;
-  if(!gObj) gObj = await swgohClient.post('guild', {guildId: pObj.guildId, includeRecentGuildActivityInfo: true}, null)
+  let gObj = await swgohClient.post('guild', {guildId: pObj.guildId, includeRecentGuildActivityInfo: true}, null)
   if(gObj?.guild) gObj = gObj.guild
-  if(mongoCache === 0 && gObj?.profile?.id){
-    gObj.updated = Date.now()
-    await mongo.set('ticketCheckCache', {_id: gObj.profile.id}, gObj)
-  }
   if(!gObj?.member) return { content : 'Error getting guild data' }
 
   let guild = (await mongo.find('guilds', {_id: pObj.guildId}))[0]
   let ticketCount = guild?.auto?.ticketCount || 600
-  await mongo.set('ticketCache', {_id: gObj.profile.id}, {member: gObj.member, updated: Date.now(), TTL: new Date(gObj.nextChallengesRefresh * 1000)})
+  let timeNow = Date.now(), resetTime = gObj.nextChallengesRefresh * 1000
+  if(resetTime > timeNow && (resetTime - timeNow) < 19 * 3600 * 1000) await mongo.set('ticketCache', {_id: gObj.profile.id}, { member: gObj.member, updated: Date.now(), TTL: new Date(gObj.nextChallengesRefresh * 1000)})
   let embedMsg = await getLowTickets(gObj, ticketCount);
   if(embedMsg) return { content: null, embeds: [embedMsg] }
   return { content: 'Error calculating data' }

cf788e1c1a092946b5b6ddd34caba0907527d088
scuba75 <scuba75@gmail.com>
2024-07-28 12:46:42 -0500

    update

--- src/helpers/replyTokenError.js
+++ src/helpers/replyTokenError.js
@@ -3,6 +3,6 @@ const log = require('logger')
 const replyMsg = require('./replyMsg')
 module.exports = async(obj = {}, allyCode)=>{
   if(allyCode) log.error('Google Token Error for allyCode: '+allyCode)
-  await replyMsg(obj, {content: 'Your google auth has been revoked or expired. Please re auth the bot using the `/allycode auth google` command'})
+  await replyMsg(obj, {content: 'Your google auth has been revoked or expired. Please re auth the bot using the `/allycode auth android` command'})
   return 'GETTING_CONFIRMATION'
 }

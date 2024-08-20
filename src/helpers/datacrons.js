30cbe56c94973c90296fb708f77c7e1444d49edf
scuba75 <scuba75@gmail.com>
2024-08-20 09:47:44 -0500

    update

--- src/helpers/datacrons.js
+++ src/helpers/datacrons.js
@@ -1,6 +1,7 @@
 'use strict'
 const mongo = require('mongoclient')
-const getDataCron = async(cron = {}, cronDef = {}, res = {})=>{
+const getDataCron = async(cron = {}, playerId, cronDef = {}, res = {})=>{
+  if(!playerId) return
   if(!cron?.affix || cron.affix?.length < 9) return
   if(!cron.affix[8]) return
   if(!cronDef[cron.templateId]) cronDef[cron.templateId] = (await mongo.find('datacronList', { _id: cron.templateId }, { id: 1, setId: 1, ability: 1, nameKey: 1 }))[0]
@@ -9,14 +10,14 @@ const getDataCron = async(cron = {}, cronDef = {}, res = {})=>{
   let unit = cronDef[cron.templateId]?.ability[cron.affix[8].abilityId]?.target[cron.affix[8].targetRule]?.unit
   if(!unit.baseId) return
   if(!res[cron.setId]) res[cron.setId] = { id: cron.setId, nameKey: cronDef[cron.templateId].nameKey, units: {} }
-  if(!res[cron.setId].units[unit.baseId]) res[cron.setId].units[unit.baseId] = { id: unit.baseId, nameKey: unit.nameKey, count: 0 }
-  res[cron.setId].units[unit.baseId].count++
+  if(!res[cron.setId].units[unit.baseId]) res[cron.setId].units[unit.baseId] = { id: unit.baseId, nameKey: unit.nameKey, players: {} }
+  res[cron.setId].units[unit.baseId].players[playerId] = playerId
 }
 const getUnitDataCrons = async(players = [], cronDef = {})=>{
   let res = {}
   for(let x in players){
     if(!players[x]?.datacron || players[x]?.datacron?.length === 0) continue
-    for(let i in players[x].datacron) await getDataCron(players[x].datacron[i], cronDef, res)
+    for(let i in players[x].datacron) await getDataCron(players[x].datacron[i], players[x].playerId, cronDef, res)
   }
   return res
 }
@@ -25,7 +26,8 @@ const getCronCounts = (home = {}, away = {})=>{
   for(let i in home){
     if(!res[i]) res[i] = { id: i, nameKey: home[i].nameKey, units: {} }
     for(let u in home[i].units){
-      res[i].units[u] = { id: u, nameKey: home[i]?.units[u]?.nameKey, home: (home[i]?.units[u]?.count || 0), away: (away[i]?.units[u]?.count || 0) }
+      let homeCount = +(Object.values(home[i]?.units[u]?.players || {})?.length || 0), awayCount = +(Object.values(away[i]?.units[u]?.players || {})?.length || 0)
+      res[i].units[u] = { id: u, nameKey: home[i]?.units[u]?.nameKey, home: homeCount, away: awayCount }
     }
   }
   for(let i in away){

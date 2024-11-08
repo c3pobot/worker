83e31be4264733176e23838ca4d91cf16ca7b3d9
scuba75 <scuba75@gmail.com>
2024-11-08 08:47:08 -0500

    update

--- src/cmds/allycode/add/verifyAccount.js
+++ src/cmds/allycode/add/verifyAccount.js
@@ -1,7 +1,7 @@
 'use strict'
 const mongo = require('mongoclient')
 const addAllyCode = require('./addAllyCode')
-const assetUrl = 'https://game-assets.swgoh.gg'
+const assetUrl = process.env.SWGOHGG_IMG_URL
 const getRandomNum = (array = []) =>{
   if(array.length == 1) return 0
   return Math.floor(Math.random()*array.length)
@@ -31,12 +31,12 @@ module.exports = async(obj = {}, opt = {}, pObj)=>{
   if(!data?.icon) return { content: `error getting info for ${newPortrait} for verification` }
   let embedMsg = {
     color: 15844367,
-    thumbnail:{
-      url: `${assetUrl}/${data.icon}.png`
-    },
     description: `allyCode **${pObj.allyCode}** for player **${pObj.name}** is already linked to another discordId\n`
   }
-  embedMsg.description += `You can verify you have access to this account by setting you profile portrait to **${data.nameKey}** (shown in this message) and running the command again.\n`
+  if(assetUrl) embedMsg.thumbnail = { url: `${assetUrl}/${data.icon}.png` }
+  embedMsg.description += `You can verify you have access to this account by setting you profile portrait to **${data.nameKey}**`
+  if(assetUrl) embedMsg.description += ' (shown in this message)'
+  embedMsg.description += ' and running the command again.\n'
   embedMsg.description += 'Note: This verification will expire in ~10 minutes'
   await mongo.set('allyCodeVerificationCache', { _id: pObj.playerId }, { dId: obj.member?.user?.id, allyCode: pObj.allyCode, playerPortrait: newPortrait })
   return { content: null, embeds: [embedMsg] }

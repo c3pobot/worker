6f39ba7e32a6287d4e9b6af5a65e88398945b648
scuba75 <scuba75@gmail.com>
2025-01-08 16:12:13 -0500

    update

--- src/cmds/p/relic/index.js
+++ src/cmds/p/relic/index.js
@@ -5,6 +5,8 @@ const { dataList } = require('src/helpers/dataList')
 const { getPlayerAC, fetchPlayer, truncateString, getRelicLevel } = require('src/helpers')
 
 module.exports = async(obj = {}, opt = {})=>{
+  let max_relic = opt.max_relic?.value, exact_match = opt.exact_match?.value
+  if(max_relic) max_relic = +max_relic + 2
   if(!dataList?.unitList) return { content: 'unitList is empty' }
 
   let allyObj = await getPlayerAC(obj, opt)
@@ -18,7 +20,7 @@ module.exports = async(obj = {}, opt = {})=>{
   let gType = opt.option?.value, gValue = opt.value?.value, sort = opt.sort?.value || 'nameKey', order = 'ascending'
   if(sort == 'gp') order = 'descending'
 
-  let { rLevel, gLevel } = getRelicLevel(opt, 7, 5)
+  let { rLevel, gLevel } = getRelicLevel(opt, 7, 13)
   if(rLevel > 0) rLevel += 2
   let units = pObj?.rosterUnit?.filter(x=>x.relic?.currentTier >= rLevel && x.currentTier >= gLevel)?.map((u)=>{
     let baseId = u?.definitionId?.split(':')[0]
@@ -30,6 +32,11 @@ module.exports = async(obj = {}, opt = {})=>{
       gp: u.gp || 0
     }
   })
+  if(exact_match){
+    units = units.filter(x=>x.gear === gLevel)
+    if(rLevel) units = units.filter(x=>x.relic === rLevel)
+  }
+  if(max_relic && rLevel && !exact_match) units = units.filter(x=>x.relic < max_relic + 1)
   if(!units) return { content: 'error getting untis' }
   if(units.length == 0) return { content: 'Player has no units that meet the requested criteria' }
 
@@ -39,8 +46,28 @@ module.exports = async(obj = {}, opt = {})=>{
     color: 15844367,
     timestamp: new Date(pObj.updated),
     title: pObj.name+' ('+units.length+')',
-    description: 'Units '+(rLevel ? ' Relic >='+(rLevel - 2):'')+(!rLevel && gLevel ? 'Gear >='+gLevel:'')+' (<UNITCOUNT>)\n```\nG/R : Unit\n'
+    description: 'Units '
   }
+  let embedDesc = 'Units '
+  if(rLevel){
+    if(exact_match){
+       embedDesc += `Relic = ${rLevel - 2}`
+    }else{
+      if(max_relic){
+        embedDesc += `${max_relic - 2} >= Relic >= ${rLevel - 2}`
+      }else{
+        embedDesc += `Relic >= ${rLevel - 2}`
+      }
+    }
+  }else{
+    if(exact_match){
+      embedDesc += `Gear = ${gLevel}`
+    }else{
+      embedDesc += `Gear >= ${gLevel}`
+    }
+  }
+  embedDesc += ` (<UNITCOUNT>)\n\`\`\`\nG/R : Unit\n`
+  embedMsg.description =  embedDesc
   let count = 0, unitCount = 0
   for(let i in units){
     if(units[i].baseId){
@@ -59,7 +86,7 @@ module.exports = async(obj = {}, opt = {})=>{
       }else{
         msg2send.content = 'There are more than 250 units that meet the criteria. I can only show 250'
       }
-      embedMsg.description = 'Units '+(rLevel ? ' Relic >='+(rLevel - 2):'')+(!rLevel && gLevel ? 'Gear >='+gLevel:'')+' (<UNITCOUNT>)\n```\nG/R : Unit\n'
+      embedMsg.description = embedDesc
       count = 0
       unitCount = 0
     }

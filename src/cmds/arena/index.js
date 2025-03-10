876f32219422de11781741c54a0d057739d5a62c
scuba75 <scuba75@gmail.com>
2025-03-10 10:31:37 -0500

    update

--- src/cmds/arena/index.js
+++ src/cmds/arena/index.js
@@ -1,6 +1,7 @@
 'use strict'
 const checkAuth = require('./checkAuth')
 const mongo = require('mongoclient')
+const { botSettings } = require('src/helpers/botSettings')
 const { replyError } = require('src/helpers')
 const Cmds = {}
 Cmds.guild = require('./guild')
@@ -8,19 +9,34 @@ Cmds.notify = require('./notify')
 Cmds.show = require('./show')
 Cmds.user = require('./user')
 
+const getPatreon = async(discordId)=>{
+  if(!discordId) return
+  let patreon = (await mongo.find('patreon', {_id: discordId }))[0]
+  if(!patreon && botSettings?.botSID){
+    let subs = (await mongo.find('serverSubscriptions', { _id: botSettings.botSID.toString() }, { guild: 1}))[0]
+    if(!subs?.guild || subs?.guild?.length == 0) return
+    if(subs.guild.includes(discordId)){
+      patreon = { _id: discordId, maxAllyCodes: 200, status: 1, subscriber: true, users: [], guilds: [] }
+      await mongo.set('patreon', { _id: discordId }, patreon)
+    }
+  }
+  return patreon
+}
+
 module.exports = async(obj = {})=>{
   try{
     let tempCmd = obj.subCmdGroup || obj.subCmd, opt = obj.data?.options || {}
-    let patreon = (await mongo.find('patreon', {_id: obj.member?.user?.id, status: 1 }))[0]
+    let patreon = await getPatreon(obj.member?.user?.id)
     let msg2send = { content: 'command not recongnized' }
     if(tempCmd === 'notify'){
-      msg2send.content = 'This is only available to patreons or those sponsored by a patreon'
+      msg2send.content = 'This is only available to subscribers, patreons or those sponsored by a patreon'
       let auth = await checkAuth(obj.member.user.id)
       if(auth) msg2send = await Cmds.notify(obj, opt)
     }
     if(tempCmd && tempCmd !== 'notify' && Cmds[tempCmd]){
-      msg2send.content = 'This is only avaliable to patreons'
-      if(patreon) msg2send = await Cmds[tempCmd](obj, patreon, opt)
+      msg2send.content = 'This is only avaliable to patreons and guild sync subscribers'
+      if(patreon && !patreon.status) msg2send.content = 'Your patreon subscription expired'
+      if(patreon?.status) msg2send = await Cmds[tempCmd](obj, patreon, opt)
     }
     return msg2send
   }catch(e){
--- src/cmds/botadmin/subscriptions.js
+++ src/cmds/botadmin/subscriptions.js
@@ -5,23 +5,25 @@ const { botSettings } = require('src/helpers/botSettings')
  module.exports = async(obj = {}, opt = {})=>{
    if(!botSettings?.botSID) return { content: 'Bot Server not defined' }
 
-   if(!opt.vip?.value && !opt.guild?.value && !opt.shard?.value) return { content: 'you must provide a subscription role...' }
+   //if(!opt.vip?.value && !opt.guild?.value && !opt.shard?.value) return { content: 'you must provide a subscription role...' }
    let data = (await mongo.find('serverSubscriptions', { _id: botSettings.botSID.toString() }, { _id: 0, TTL: 0 }))[0]
    if(!data?.id) data = { id: botSettings.botSID.toString(), vip: [], guild: [], shard: [] }
    let msg2send = `Server subscriptions role(s):\n`
    if(opt.vip?.value){
      data.vipRole = opt.vip.value
-     msg2send += `vip to **@${opt.vip?.data?.name}**\n`
+     data.vipRoleName = opt.vip?.data?.name
    }
    if(opt.guild?.value){
      data.guildRole = opt.guild.value
-     msg2send += `guild to **@${opt.guild?.data?.name}**\n`
+     data.guildRoleName = opt.guild?.data?.name
    }
    if(opt.shard?.value){
      data.shardRole = opt.shard.value
-     msg2send += `shard to **@${opt.shard?.data?.name}**\n`
+     data.shardRoleName = opt.shard?.data?.name
    }
-
+   if(data.vipRoleName) msg2send += `vip to **@${data.vipRoleName}**\n`
+   if(data.guildRoleName) msg2send += `guild to **@${data.guildRoleName}**\n`
+   if(data.shardRoleName) msg2send += `shard to **@${data.shardRoleName}**\n`
    await mongo.set('serverSubscriptions', { _id: botSettings.botSID.toString() }, data)
    return { content: msg2send }
  }

5404d0d1b6d5d6842685d5d897a8dfc8af53eab4
scuba75 <scuba75@gmail.com>
2025-04-17 10:32:25 -0500

    update

--- src/cmds/shardadmin/rotation/set.js
+++ src/cmds/shardadmin/rotation/set.js
@@ -2,10 +2,10 @@
 const mongo = require('mongoclient')
 const { getOptValue, getDiscordAC } = require('src/helpers')
 const swgohClient = require('src/swgohClient')
-
+const { showRotationSchedule } = require('src/helpers')
 module.exports = async(obj = {}, shard = {}, opt = {})=>{
   let msg2send = {content: 'You did not provide the correct information'}, pObj, allyCode, dId
-  let schedule = getOptValue(opt, 'schedule')?.toUpperCase()
+    let schedule = opt?.schedule?.value?.toString()?.toUpperCase()
   if(!schedule) return { content: 'you did not provide a rotation schedule name'}
   let tempRot = {
     id: schedule,
@@ -21,19 +21,22 @@ module.exports = async(obj = {}, shard = {}, opt = {})=>{
   }
   let rots = (await mongo.find('shardRotations', {_id: shard._id}))[0]
   if(rots && rots[schedule]) tempRot = rots[schedule]
-  tempRot.chId = getOptValue(opt, 'channel', tempRot.chId)
-  tempRot.startTime = getOptValue(opt, 'hours', tempRot.startTime)
-  tempRot.notify = getOptValue(opt, 'notify', tempRot.notify)
-  tempRot.order = getOptValue(opt, 'order', tempRot.order)
-  let role = getOptValue(opt, 'role')
+  tempRot.chId = opt?.channel?.value || tempRot.chId
+  tempRot.startTime = opt?.hours?.value || tempRot.startTime
+  tempRot.notify = opt?.notify?.value || tempRot.notify
+  tempRot.order = opt?.order?.value || tempRot.order
+
+  let role = opt.role?.value
+
   if(role){
     if(role.startsWith('<@&')){
-      tempRot.roleId = opt.find(x=>x.name == 'role').value.replace(/[<@&>]/g, '')
+      tempRot.roleId = opt.role?.value?.replace(/[<@&>]/g, '')
     }else{
       delete tempRot.roleId
     }
   }
-  let poTime = getOptValue(opt, 'potime')
+
+  let poTime = opt?.potime?.value
   if(poTime?.startsWith('<@')){
     dId = poTime.replace(/[<@!>]/g, '')
   }else{
@@ -50,6 +53,6 @@ module.exports = async(obj = {}, shard = {}, opt = {})=>{
   }
   await mongo.set('shardRotations', {_id: shard._id}, {[schedule]: tempRot})
   msg2send.content = await showRotationSchedule(obj, tempRot, shard, true)
-  if(tempRot.poOffSet != null) msg2send.content += '**Note: There is no poOffSet for the rotation schedule.\nYou need to provide an allyCode or mention a user that has allyCode linked to discordId**'
+  if(!tempRot.poOffSet) msg2send.content += '**Note: There is no poOffSet for the rotation schedule.\nYou need to provide an allyCode or mention a user that has allyCode linked to discordId**'
   return msg2send
 }
